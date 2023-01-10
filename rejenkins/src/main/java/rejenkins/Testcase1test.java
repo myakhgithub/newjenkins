42c26aedd4e5228b6674176e5d42982672606b62
Feroz <Feroz@DESKTOP-UVE6BKF>
2023-01-10 12:22:37 +0530

    first commit

---
+++ rejenkins/src/main/java/rejenkins/Testcase1test.java
@@ -0,0 +1,13 @@
+package rejenkins;
+
+import org.testng.annotations.Test;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Testcase1test {
+	@Test
+	public void m1()
+	{
+		Reporter.log("Hello",true);
+	}
+}
---
+++ rejenkins/src/main/java/rejenkins/Testcase2test.java
@@ -0,0 +1,13 @@
+package rejenkins;
+
+import org.testng.annotations.Test;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Testcase2test {
+	@Test
+	public void m2()
+	{
+		Reporter.log("Hii",true);
+	}
+}
